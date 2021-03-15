222856709c775a2f8df3fe7479836680d886bfa3
jlcarruda <jlcarruda3@gmail.com>
2021-03-15 18:00:08 -0300

    added logging of bucket name and object key for debugging and future reference

--- serverless-poc/handler.js
+++ serverless-poc/handler.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports.hello = async (event) => {
-  console.log("Triggered Event", event)
+  // console.log("Triggered Event", JSON.stringify(event.Records[0].s3) )
+  const eventRecord = event.Records[0]
+  const srcBucket = eventRecord.s3.bucket.name
+  const srcKey = decodeURIComponent(eventRecord.s3.object.key.replace(/\+/g, " "))
+
+  console.log(`S3 event triggered: ${eventRecord.eventName}`)
+  console.log(`S3 Bucket name: ${srcBucket}`)
+  console.log(`S3 Object Key: ${srcKey}`)
 };
